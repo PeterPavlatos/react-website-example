b45881b606efe893310a23f309317e7122b0103b
Peter <pavlatos.peter@gmail.com>
2019-06-25 06:33:38 -0400

    Added files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h1 className="center">Contact</h1>
+            <p>Lorem    </p>
+        </div>
+    )
+}
+export default Contact;
